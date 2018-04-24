b07c2f9007cb742986108ef5a077e36e3f0e2135
MrErin <erinmeaker@gmail.com>
2018-04-24 08:28:13 -0500

    added chaining methods

---
+++ MapFilterReduce/chainingMethods.js
@@ -0,0 +1,19 @@
+const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8]
+
+//Using one single line of code, complete the following tasks: 
+//Sort the numbers in descending order(10, 9, 8, 7, etc).
+
+console.log(integers.sort((a, b) => b - a))
+
+
+// Remove any integers greater than 19.
+
+console.log(integers.filter(integer => integer < 19 ))
+
+// Multiply each remaining number by 1.5 and then subtract 1.
+
+console.log(integers.map((integer) => (integer * 1.5) - 1))
+
+// Then output(either in the DOM or the console) the sum of all the resulting numbers.
+
+console.log(integers.reduce((currentTotal, next) => currentTotal + next))
